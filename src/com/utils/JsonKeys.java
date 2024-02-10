eebb19951c3007b56a4ea1d21b7030e39504f478
Orlando Parente <parenteorlando@yahoo.it>
2024-02-11 00:51:46 +0100

    Update JsonKeys.java

--- src/com/utils/JsonKeys.java
+++ src/com/utils/JsonKeys.java
@@ -4,15 +4,5 @@ public class JsonKeys {
 
 	public final static String QUESTION = "question";
 	public final static String ANSWERS = "answers";
-	
-	// SERVONO ? --------------------------------------
-	
-	public final static String ANSWER1 = "0";
-	public final static String ANSWER2 = "1";
-	public final static String ANSWER3 = "2";
-	public final static String ANSWER4 = "3";
-	
-	// ------------------------------------------------
-	
 	public final static String RIGHT_ANSWERS = "right_answers";
 }
